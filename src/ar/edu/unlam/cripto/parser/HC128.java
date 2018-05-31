4b2bb909aca29cbf9cd89b72f2353c977c7ac234
Pablo Becerra <pablo_becerra94@yahoo.com.ar>
2018-05-31 19:36:55 -0300

    funciones complementarias realizadas

--- src/ar/edu/unlam/cripto/parser/HC128.java
+++ src/ar/edu/unlam/cripto/parser/HC128.java
@@ -13,16 +13,16 @@ public class HC128 {
 
 	// Cada tabla de 512 filas de 32 bits - [512] con cada BigInteger de 32 bits.
 	private byte[] key, iv;
-    private int[] p = new int[512];
-    private int[] q = new int[512];
-    private int count = 0;
-    private byte[] buffer = new byte[4];
-    private int index = 0;
+	private int[] p = new int[512];
+	private int[] q = new int[512];
+	private int count = 0;
+	private byte[] buffer = new byte[4];
+	private int index = 0;
 
-	public HC128(byte[] iv,byte[]key){
+	public HC128(byte[] iv, byte[] key) {
 		this.iv = iv;
-        this.key = key;
-        inicializar();
+		this.key = key;
+		inicializar();
 	}
 
 	/*
@@ -37,45 +37,41 @@ public class HC128 {
 	 */
 	public void inicializar() {
 		if (key.length != 16) {
-            throw new java.lang.IllegalArgumentException(
-                    "La key debe tener 128 bits");
-        }
-        count = 0;
-        int[] w = new int[1280];
-        
-        byte primerByteKey =key[0];
-        
-        String[] s1 = String.format("%8s", Integer.toBinaryString(primerByteKey & 0xFF)).replace(' ', '0').split("");
-        
-        for(int i=0; i<=7; i++) {
-        	w[i] = Integer.parseInt(s1[i]);
-        }
-        
-        byte  primerByteIV =iv[0];
-        String[] s2 = String.format("%8s", Integer.toBinaryString(primerByteIV & 0xFF)).replace(' ', '0').split("");
-        
-        for(int i=8; i<=15; i++) {
-        	w[i] = Integer.parseInt(s2[i]);
-        }
-        
-        
-        //TODO: Cambiar según pseudocodigo
-        for(int i=16; i<=1279; i++) {
-//         	 w[i] = f2(w[i - 2]) + w[i - 7] + f1(w[i - 15]) + w[i - 16] + i;
-        }
-        
-        
-        for(int i=0; i<=511; i++) {
-        	p[i] = w[i+256];
-        	q[i] = w[i+768];
-        }
-        //TODO: Cambiar según pseudocodigo
-        for(int i=0; i<=511; i++) {
-   
-//        	p[i] = w[i+256];
-//        	q[i] = w[i+768];
-        }
-        	
+			throw new java.lang.IllegalArgumentException("La key debe tener 128 bits");
+		}
+		count = 0;
+		int[] w = new int[1280];
+
+		byte primerByteKey = key[0];
+
+		String[] s1 = String.format("%8s", Integer.toBinaryString(primerByteKey & 0xFF)).replace(' ', '0').split("");
+
+		for (int i = 0; i <= 7; i++) {
+			w[i] = Integer.parseInt(s1[i]);
+		}
+
+		byte primerByteIV = iv[0];
+		String[] s2 = String.format("%8s", Integer.toBinaryString(primerByteIV & 0xFF)).replace(' ', '0').split("");
+
+		for (int i = 8; i <= 15; i++) {
+			w[i] = Integer.parseInt(s2[i]);
+		}
+
+		for (int i = 16; i <= 1279; i++) {
+			w[i] = f2(w[i - 2]) + w[i - 7] + f1(w[i - 15]) + w[i - 16] + i;
+		}
+
+		for (int i = 0; i <= 511; i++) {
+			p[i] = w[i + 256];
+			q[i] = w[i + 768];
+		}
+		// TODO: Cambiar según pseudocodigo
+		for (int i = 0; i <= 511; i++) {
+
+			// p[i] = w[i+256];
+			// q[i] = w[i+768];
+		}
+
 	}
 
 	public void generarEspacioDeLlaves() {
@@ -91,29 +87,30 @@ public class HC128 {
 	/*
 	 * Funciones varias de apoyo al algoritmo principal
 	 */
-	public void f1(EntidadAParsear entidad) {
-		// String s = null;
-		// BitSet bits = entidad.getCadenaDeBits();
-		// BitSet bitsCambiados = (bits >> 7) ^ (bits >> 18) ^ (bits >> 3);
+	public int f1(int x) {
+		return Integer.rotateRight(x, 7) ^ Integer.rotateRight(x, 18) ^ (x >>> 3);
 	}
 
-	public void f2() {
-
+	public int f2(int x) {
+		return Integer.rotateRight(x, 17) ^ Integer.rotateRight(x, 19) ^ (x >>> 10);
 	}
 
-	public void g1() {
-
+	public int g1(int x, int y, int z) {
+		return (Integer.rotateRight(x, 10) ^ Integer.rotateRight(z, 23)) + Integer.rotateRight(y, 8);
 	}
 
-	public void g2() {
-
+	public int g2(int x, int y, int z) {
+		return (Integer.rotateLeft(x, 10) ^ Integer.rotateLeft(z, 23)) + Integer.rotateLeft(y, 8);
 	}
 
-	public void h1() {
+	
+	
+//	public int h1(int x) {
+//		return q[x & 0xFF] + q[((x >> 16) & 0xFF) + 256];
+//	}
 
-	}
+//	public int h2(int x) {
+//		return p[x & 0xFF] + p[((x >> 16) & 0xFF) + 256];
+//	}
 
-	public void h2() {
-
-	}
 }
